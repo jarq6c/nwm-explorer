dcd72c066a486ff6c266a61d511267fb89ee1415
jason.regina <jason.regina@noaa.gov>
2025-06-16 14:17:26 +0000

    add pairs reader

--- src/nwm_explorer/readers.py
+++ src/nwm_explorer/readers.py
@@ -4,9 +4,63 @@ from dataclasses import dataclass
 from typing import TypedDict
 import numpy as np
 import pandas as pd
+import polars as pl
 import plotly.graph_objects as go
 
-from nwm_explorer.mappings import Domain, Configuration, Metric, Confidence
+from nwm_explorer.mappings import (Domain, Configuration, Metric, Confidence,
+    NWM_URL_BUILDERS, FileType, Variable, Units)
+from nwm_explorer.urls import generate_reference_dates
+from nwm_explorer.data import generate_filepath
+from nwm_explorer.logger import get_logger
+
+def read_pairs(
+        root: Path,
+        start_date: pd.Timestamp,
+        end_date: pd.Timestamp
+) -> dict[tuple[Domain, Configuration], pl.LazyFrame]:
+    """
+    Find and lazily load paired data.
+
+    Parameters
+    ----------
+    root: Path, required
+        Root directory to save downloaded and processed files.
+    start_date: pd.Timestamp
+        First date to start retrieving data.
+    end_date: pd.Timestamp
+        Last date to retrieve data.
+    
+    Returns
+    -------
+    dict[tuple[Domain, Configuration], pl.LazyFrame]
+    """
+    logger = get_logger("nwm_explorer.readers.read_pairs")
+    # Process model output
+    logger.info("Scanning pairs")
+    reference_dates = generate_reference_dates(start_date, end_date)
+    pairs = {}
+    for (domain, configuration), _ in NWM_URL_BUILDERS.items():
+        day_files = []
+        for rd in reference_dates:
+            # Check for file existence
+            parquet_file = generate_filepath(
+                root, FileType.parquet, configuration, Variable.streamflow_pairs,
+                Units.cubic_feet_per_second, rd
+            )
+            if parquet_file.exists():
+                logger.info(f"Found existing file: {parquet_file}")
+                day_files.append(parquet_file)
+                continue
+            
+        # Check for at least one file
+        if not day_files:
+            logger.info(f"Found no data for {domain} {configuration}")
+            continue
+        
+        # Merge files
+        logger.info(f"Merging parquet files")
+        pairs[(domain, configuration)] = pl.scan_parquet(day_files)
+    return pairs
 
 class FigurePatch(TypedDict):
     """
