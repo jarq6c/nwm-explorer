d6fd1ea90ff7bcb475b0e37105572848375803d0
jason.regina <jason.regina@noaa.gov>
2025-07-01 15:12:14 +0000

    maintain layout when refreshing

--- site_map.py
+++ site_map.py
@@ -21,9 +21,9 @@ class SiteMapCard:
             default_zoom: int
             ):
         # Hover template
-        hover_template = ""
+        self.hover_template = ""
         for idx, c in enumerate(custom_labels):
-            hover_template += f"{c}: %" + "{customdata[" + str(idx) + "]}<br>"
+            self.hover_template += f"{c}: %" + "{customdata[" + str(idx) + "]}<br>"
 
         # Make map
         data = [go.Scattermap(
@@ -44,7 +44,7 @@ class SiteMapCard:
             lon=longitude,
             customdata=custom_data,
             hovertemplate=(
-                hover_template +
+                self.hover_template +
                 "Longitude: %{lon}<br>"
                 "Latitude: %{lat}"
                 f"<br>{value_label}: "
@@ -54,6 +54,13 @@ class SiteMapCard:
             name="",
             mode="markers"
         )]
+
+        # Default map options
+        self.map_center = {
+            "lon": np.mean(longitude),
+            "lat": np.mean(latitude)
+        }
+        self.map_zoom = default_zoom
 
         # Create card
         self.card = PlotlyCard(
@@ -65,11 +72,8 @@ class SiteMapCard:
                 margin=dict(l=0, r=0, t=0, b=0),
                 map=dict(
                     style="satellite-streets",
-                    center=dict(
-                        lat=np.mean(latitude),
-                        lon=np.mean(longitude)
-                    ),
-                    zoom=default_zoom
+                    center=self.map_center,
+                    zoom=self.map_zoom
                 ),
                 clickmode="event",
                 modebar=dict(
@@ -106,11 +110,21 @@ class SiteMapCard:
             self.lat_min = data["map._derived"]["coordinates"][2][1]
             self.lon_max = data["map._derived"]["coordinates"][1][0]
             self.lon_min = data["map._derived"]["coordinates"][0][0]
+            self.map_center = data["map.center"]
+            self.map_zoom = data["map.zoom"]
         elif "map.center" in data:
             self.lat_min = np.min(self.card.data[0].lat)
             self.lat_max = np.max(self.card.data[0].lat)
             self.lon_min = np.min(self.card.data[0].lon)
             self.lon_max = np.max(self.card.data[0].lon)
+            self.map_center = data["map.center"]
+            self.map_zoom = data["map.zoom"]
+
+        # Re-center map
+        self.card.recenter_map(dict(
+            center=self.map_center,
+            zoom=self.map_zoom
+        ))
         
     def update_selection(self, data: dict) -> None:
         custom_data = data["points"][0]["customdata"]
@@ -121,7 +135,7 @@ class SiteMapCard:
         self.selection["lat"] = data["points"][0]["lat"]
     
     def update_points(
-            self, 
+            self,
             latitude: npt.ArrayLike,
             longitude: npt.ArrayLike,
             custom_data: pl.DataFrame,
@@ -132,9 +146,9 @@ class SiteMapCard:
             default_zoom: int
         ) -> None:
         # Hover template
-        hover_template = ""
+        self.hover_template = ""
         for idx, c in enumerate(custom_labels):
-            hover_template += f"{c}: %" + "{customdata[" + str(idx) + "]}<br>"
+            self.hover_template += f"{c}: %" + "{customdata[" + str(idx) + "]}<br>"
         
         # Data
         self.card.update_data(dict(
@@ -142,7 +156,7 @@ class SiteMapCard:
             lon=longitude,
             customdata=custom_data,
             hovertemplate=(
-                hover_template +
+                self.hover_template +
                 "Longitude: %{lon}<br>"
                 "Latitude: %{lat}"
                 f"<br>{value_label}: "
@@ -162,13 +176,17 @@ class SiteMapCard:
             cmax=value_limits[1]
         ))
 
+        # Default map options
+        self.map_center = {
+            "lon": np.mean(longitude),
+            "lat": np.mean(latitude)
+        }
+        self.map_zoom = default_zoom
+
         # Re-center map
         self.card.recenter_map(dict(
-            center={
-                "lat": np.mean(latitude),
-                "lon": np.mean(longitude)
-                },
-            zoom=default_zoom
+            center=self.map_center,
+            zoom=self.map_zoom
         ))
 
         # Update boundaries
@@ -180,6 +198,35 @@ class SiteMapCard:
         # Click data
         self.columns = custom_data.columns
         self.selection = {}
+    
+    def update_values(
+            self,
+            values: npt.ArrayLike,
+            value_label: str,
+            value_limits: tuple[float, float]
+        ) -> None:
+        # Data
+        self.card.update_data(dict(
+            hovertemplate=(
+                self.hover_template +
+                "Longitude: %{lon}<br>"
+                "Latitude: %{lat}"
+                f"<br>{value_label}: "
+                "%{marker.color:.2f}"
+            )
+        ))
+
+        # Markers
+        self.card.update_markers(dict(
+            color=values,
+            colorbar=dict(
+                title=dict(
+                    text=value_label
+                    )
+                ),
+            cmin=value_limits[0],
+            cmax=value_limits[1]
+        ))
 
     def servable(self) -> pn.Card:
         return self.card.servable()
@@ -243,7 +290,17 @@ def main():
     )
     card.refresh()
 
-    pn.serve(card.servable())
+    rng = np.random.default_rng()
+    def update_colors(event):
+        if not event:
+            return
+        gain = rng.uniform(0.0, 1.0)
+        card.update_values([values[0]*gain], f"GAIN ({gain:.2f})", (0.0, gain))
+        card.refresh()
+    button = pn.widgets.Button(name="Click me", button_type="primary")
+    pn.bind(update_colors, button, watch=True)
+
+    pn.serve(pn.Column(card.servable(), button))
 
 if __name__ == "__main__":
     main()
