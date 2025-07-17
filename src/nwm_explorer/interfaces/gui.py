b65732bc4ea1d21c794e7a171f25158c126190c3
jason.regina <jason.regina@noaa.gov>
2025-07-17 19:06:39 +0000

    start mocking up filters

--- src/nwm_explorer/interfaces/gui.py
+++ src/nwm_explorer/interfaces/gui.py
@@ -2,11 +2,24 @@
 from pathlib import Path
 import inspect
 
+import polars as pl
 import panel as pn
 from panel.template import BootstrapTemplate
 
 from nwm_explorer.logging.logger import get_logger
 from nwm_explorer.evaluation.compute import EvaluationRegistry
+from nwm_explorer.data.mapping import ModelDomain, ModelConfiguration
+
+PRETTY_DOMAINS: dict[ModelDomain, str] = {
+    ModelDomain.alaska: "Alaska",
+    ModelDomain.conus: "CONUS",
+    ModelDomain.hawaii: "Hawaii",
+    ModelDomain.puertorico: "Puerto Rico"
+}
+"""Mapping from ModelDomain to pretty strings."""
+
+DOMAIN_LOOKUP: dict[str, ModelDomain] = {v: k for k, v in PRETTY_DOMAINS.items()}
+"""Reverse look-up from pretty strings to ModelDomain."""
 
 class Dashboard:
     """Build a dashboard for exploring National Water Model output."""
@@ -29,7 +42,45 @@ class Dashboard:
             self.template.main.append(pn.pane.Markdown("# Registry not found. Have you run an evaluation?"))
             return
         
-        print(evaluation_registry)
+        # Scan evaluation data
+        self.data: dict[str, dict[ModelDomain, dict[ModelConfiguration, pl.LazyFrame]]] = {}
+        for label, evaluation_spec in evaluation_registry.evaluations.items():
+            self.data[label] = {}
+            for domain, files in evaluation_spec.files.items():
+                self.data[label][domain] = {}
+                for configuration, ifile in files.items():
+                    logger.info(f"Scanning {ifile}")
+                    self.data[label][domain][configuration] = pl.scan_parquet(ifile)
+
+        # Setup widgets
+        self.evaluation_filter = pn.widgets.Select(
+            name="Evaluation",
+            options=list(self.data.keys())
+        )
+        self.domain_filter = pn.widgets.Select(
+            name="Domain",
+            options=[]
+        )
+        self.configuration_filter = pn.widgets.Select(
+            name="Configuration",
+            options=[]
+        )
+
+        # Update domain
+        def update_domains(eval) -> None:
+            self.domain_filter.options = [PRETTY_DOMAINS[v] for v in self.data[eval].values()]
+        pn.bind(update_domains, self.evaluation_filter, watch=True)
+
+        # Layout
+        filters = pn.Card(pn.Column(
+            self.evaluation_filter,
+            self.domain_filter,
+            self.configuration_filter
+            ),
+            title="Filters",
+            collapsible=False
+        )
+        self.template.main.append(filters)
     
     def servable(self) -> BootstrapTemplate:
         return self.template
