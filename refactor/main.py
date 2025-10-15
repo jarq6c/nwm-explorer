00e8c21b7b6b622af746243bbc51fb88239e2b92
jason.regina <jason.regina@noaa.gov>
2025-10-15 19:32:11 +0000

    start pairing

--- refactor/main.py
+++ refactor/main.py
@@ -1,48 +1,101 @@
 """Main."""
 from pathlib import Path
+from dataclasses import dataclass
 
 import pandas as pd
 import polars as pl
 
 from modules.routelink import download_routelink
-from modules.nwm import scan_nwm, ModelConfiguration, load_nwm_site, load_nwm, download_nwm
-from modules.usgs import download_usgs, load_usgs_site
+from modules.nwm import scan_nwm, ModelConfiguration
+from modules.usgs import scan_usgs
+
+@dataclass
+class NWMGroupSpecification:
+    """
+    A dataclass that holds specifications for time-based polars groupby
+    operations.
+    """
+    index_column: str = "value_time"
+    group_by_columns: list[str] | None = None
+    window_interval: str = "1d"
+
+    def __post_init__(self) -> None:
+        if self.group_by_columns is None:
+            self.group_by_columns = ["nwm_feature_id", "reference_time"]
+
+    @property
+    def select_columns(self) -> list[str]:
+        """A list of columns to select."""
+        return self.group_by_columns + [self.index_column, "predicted_cfs"]
+
+    @property
+    def sort_columns(self) -> list[str]:
+        """A list of columns to sort by."""
+        return self.group_by_columns + [self.index_column]
+
+GROUP_SPECIFICATIONS: dict[ModelConfiguration, NWMGroupSpecification] = {
+    ModelConfiguration.ANALYSIS_ASSIM_EXTEND_ALASKA_NO_DA: NWMGroupSpecification(),
+    ModelConfiguration.ANALYSIS_ASSIM_EXTEND_NO_DA: NWMGroupSpecification(),
+    ModelConfiguration.ANALYSIS_ASSIM_HAWAII_NO_DA: NWMGroupSpecification(),
+    ModelConfiguration.ANALYSIS_ASSIM_PUERTO_RICO_NO_DA: NWMGroupSpecification(),
+    ModelConfiguration.MEDIUM_RANGE_MEM_1: NWMGroupSpecification(),
+    ModelConfiguration.MEDIUM_RANGE_BLEND: NWMGroupSpecification(),
+    ModelConfiguration.MEDIUM_RANGE_NO_DA: NWMGroupSpecification(),
+    ModelConfiguration.MEDIUM_RANGE_ALASKA_MEM_1: NWMGroupSpecification(),
+    ModelConfiguration.MEDIUM_RANGE_BLEND_ALASKA: NWMGroupSpecification(),
+    ModelConfiguration.MEDIUM_RANGE_ALASKA_NO_DA: NWMGroupSpecification(),
+    ModelConfiguration.SHORT_RANGE: NWMGroupSpecification(),
+    ModelConfiguration.SHORT_RANGE_ALASKA: NWMGroupSpecification(),
+    ModelConfiguration.SHORT_RANGE_HAWAII: NWMGroupSpecification(),
+    ModelConfiguration.SHORT_RANGE_HAWAII_NO_DA: NWMGroupSpecification(),
+    ModelConfiguration.SHORT_RANGE_PUERTO_RICO: NWMGroupSpecification(),
+    ModelConfiguration.SHORT_RANGE_PUERTO_RICO_NO_DA: NWMGroupSpecification(),
+}
+"""Mapping from ModelConfiguration to group-by specifications."""
 
 if __name__ == "__main__":
     root = Path("/ised/nwm_explorer_data")
 
+    # Predictions
+    predictions = scan_nwm(root)
+    date_range = pd.date_range(start="2023-10-01", end="2025-09-30", freq="1MS")
+    for configuration in list(ModelConfiguration):
+        for reference_date in date_range:
+            print(configuration, reference_date)
+            pairs = predictions.filter(
+                pl.col("configuration") == configuration,
+                pl.col("year") == reference_date.year,
+                pl.col("month") == reference_date.month
+            ).select(
+                ["nwm_feature_id", "reference_time", "value_time", "predicted_cfs"]
+            ).collect().sort(
+                ["nwm_feature_id", "reference_time", "value_time"]
+            ).group_by_dynamic(
+                "value_time",
+                every="1d",
+                group_by=["nwm_feature_id", "reference_time"]
+            ).agg(
+                pl.col("predicted_cfs").min().alias("predicted_cfs_min"),
+                pl.col("predicted_cfs").max().alias("predicted_cfs_max"),
+                # pl.col("value_time").min().alias("value_time_min"),
+                # pl.col("value_time").max().alias("value_time_max")
+            )
+            print(pairs)
+            break
+        break
+
     # Load routelink
-    rl = download_routelink(
-        root
-    ).select(
-        ["nwm_feature_id", "usgs_site_code", "domain"]
-    ).collect()
-    print(rl)
+    # rl = download_routelink(
+    #     root
+    # ).select(
+    #     ["nwm_feature_id", "usgs_site_code", "domain"]
+    # ).collect()
 
-    # Download and process NWM output
-    download_nwm(
-        start=pd.Timestamp("2025-07-14"),
-        end=pd.Timestamp("2025-09-30"),
-        root=root,
-        routelink=rl,
-        jobs=18,
-        retries=1
-    )
-
-    # Load predictions
-    predictions = scan_nwm(root, True)
-
-    # Download and process USGS streamflow
-    # download_usgs(
-    #     start=pd.Timestamp("2025-10-01"),
-    #     end=pd.Timestamp("2025-10-12"),
-    #     root=root
-    # )
-
-    # # Prepare observations
+    # Prepare observations
     # observations = scan_usgs(root
     #     ).filter(
-    #         pl.col("usgs_site_code").is_in(rl["usgs_site_code"].implode())
+    #         # pl.col("usgs_site_code").is_in(rl["usgs_site_code"].implode())
+    #         pl.col("usgs_site_code") == "02146470"
     #     ).select(
     #         ["usgs_site_code", "value_time", "observed_cfs"]
     #     ).unique(
@@ -61,6 +114,7 @@ if __name__ == "__main__":
     #             rl["nwm_feature_id"].implode()
     #         )
     #     )
+    # print(observations.head().collect())
 
     # predictions = scan_nwm(root
     #     ).filter(
