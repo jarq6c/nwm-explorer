1d6ab0cea1da5d57335216b01e296c7eba4ed38b
jason.regina <jason.regina@noaa.gov>
2025-10-20 16:34:19 +0000

    iterate over model configs

--- refactor/main.py
+++ refactor/main.py
@@ -1,18 +1,20 @@
 """Methods to evaluate pairs."""
 from pathlib import Path
+import inspect
 
 import polars as pl
 import pandas as pd
 
 from modules.nwm import ModelConfiguration
-from modules.pairs import scan_pairs
+from modules.pairs import scan_pairs, GROUP_SPECIFICATIONS
+from modules.logger import get_logger
 
-def generate_forecast_pools(
+def generate_prediction_pools(
         root: Path,
         configuration: ModelConfiguration,
         start_time: pd.Timestamp,
         end_time: pd.Timestamp,
-        lead_time_scale: int
+        lead_time_interval: int
         ) -> pl.DataFrame:
     """
     Load and group forecast pairs into lead time pools.
@@ -27,10 +29,15 @@ def generate_forecast_pools(
         First value time.
     end_time: pandas.Timestamp
         Last value time.
-    lead_time_scale: int
+    lead_time_interval: int
         Lead time scale to aggregate over in hours.
     """
+    # Get logger
+    name = __loader__.name + "." + inspect.currentframe().f_code.co_name
+    logger = get_logger(name)
+
     # Load data
+    logger.info("Loading pairs")
     dataframes = []
     for m in pd.date_range(start_time, end_time, freq="1MS"):
         dataframes.append(
@@ -44,11 +51,12 @@ def generate_forecast_pools(
         )
 
     # Compute lead time and pool pairs
+    logger.info("Pooling pairs")
     return pl.concat(
         dataframes
     ).with_columns(
         lead_time_pool=(pl.col("predicted_value_time_min").sub(pl.col("reference_time")) /
-            pl.duration(hours=lead_time_scale)).floor().cast(pl.Int32)
+            pl.duration(hours=lead_time_interval)).floor().cast(pl.Int32)
     ).sort([
         "configuration",
         "nwm_feature_id",
@@ -56,7 +64,7 @@ def generate_forecast_pools(
         "value_time"]
     ).group_by_dynamic(
         "value_time",
-        every="1d",
+        every=f"{lead_time_interval}h",
         group_by=["configuration", "nwm_feature_id", "lead_time_pool"]
     ).agg(
         pl.col("predicted_cfs_min").min(),
@@ -76,14 +84,18 @@ def generate_forecast_pools(
 
 def main() -> None:
     """Main."""
-    df = generate_forecast_pools(
-        root=Path("/ised/nwm_explorer_data"),
-        configuration=ModelConfiguration.MEDIUM_RANGE_MEM_1,
-        start_time=pd.Timestamp("2023-10-01"),
-        end_time=pd.Timestamp("2023-12-31"),
-        lead_time_scale=24
-    )
-    print(df)
+    # Process each configuration
+    for config, specs in GROUP_SPECIFICATIONS.items():
+        print(config)
+        df = generate_prediction_pools(
+            root=Path("/ised/nwm_explorer_data"),
+            configuration=config,
+            start_time=pd.Timestamp("2023-10-01"),
+            end_time=pd.Timestamp("2023-12-31"),
+            lead_time_interval=specs.window_interval
+        )
+        print(df.estimated_size("mb"))
+        break
 
 if __name__ == "__main__":
     main()
