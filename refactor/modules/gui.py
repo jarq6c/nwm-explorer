a181b94d352fed9c05d46d4fa73e27a4e82a9fd6
jason.regina <jason.regina@noaa.gov>
2025-11-18 19:20:38 +0000

    add gui modules

---
+++ refactor/modules/gui.py
@@ -0,0 +1,36 @@
+"""Basic functions to build and launch dashboards."""
+from pathlib import Path
+
+import panel as pn
+
+from .dashboard import Dashboard
+
+def generate_dashboard(
+        root: Path,
+        title: str
+        ) -> Dashboard:
+    """Instantiate and return a dashboard."""
+    return Dashboard(root, title)
+
+def generate_dashboard_closure(
+        root: Path,
+        title: str
+        ) -> Dashboard:
+    """Build and return a closure function that generates new dashboards."""
+    def closure():
+        return generate_dashboard(root, title)
+    return closure
+
+def serve_dashboard(
+        root: Path,
+        title: str
+        ) -> None:
+    """Serve dashboards."""
+    # Slugify title
+    slug = title.lower().replace(" ", "-")
+
+    # Serve
+    endpoints = {
+        slug: generate_dashboard_closure(root, title)
+    }
+    pn.serve(endpoints)
