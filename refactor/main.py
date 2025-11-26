3e78028d0248675af4b5802bf92d1eb68a0298fa
jason.regina <jason.regina@noaa.gov>
2025-11-26 17:04:00 +0000

    add export command

--- refactor/main.py
+++ refactor/main.py
@@ -1,15 +1,20 @@
 """Command-line Interface."""
 from pathlib import Path
 from datetime import datetime
+from typing import Literal
+from sys import stdout
 
 import typer
 import pandas as pd
+import polars as pl
 
 from modules.configuration import load_configuration
 from modules.nwm import download_nwm
 from modules.usgs import download_usgs
 from modules.pairs import pair_nwm_usgs
 from modules.evaluate import evaluate as run_evaluation
+from modules.evaluate import scan_evaluations
+from modules.constants import ModelConfiguration, Metric
 from modules.gui import serve_dashboards
 
 app = typer.Typer()
@@ -143,6 +148,64 @@ def evaluate(configuration: Path = Path("config.json")) -> None:
         )
 
 @app.command()
+def export(
+        root: Path,
+        label: str,
+        configuration: ModelConfiguration,
+        metric: Metric,
+        output: typer.FileTextWrite | None = None,
+        lead_time_hours_min: int = 0,
+        rank: Literal["min", "median", "max"] = "median",
+        additional_columns: tuple[str] | None = None
+    ) -> None:
+    """
+    Export evaluation metrics to CSV.
+    """
+    # Set output
+    if output is None:
+        output = stdout
+
+    # Set additional columns
+    if additional_columns is None:
+        additional_columns = [
+            "label",
+            "configuration",
+            "nwm_feature_id",
+            "usgs_site_code",
+            "reference_time_min",
+            "reference_time_max",
+            "lead_time_hours_min",
+            "predicted_value_time_min",
+            "predicted_value_time_max",
+            f"predicted_cfs_{rank}",
+            "observed_value_time_min",
+            "observed_value_time_max",
+            f"observed_cfs_{rank}",
+            "sample_size"
+        ]
+
+    # Load metrics
+    data = scan_evaluations(
+        root
+    ).filter(
+        pl.col("label") == label,
+        pl.col("configuration") == configuration,
+        pl.col("lead_time_hours_min") == lead_time_hours_min
+    ).select(
+        additional_columns + [
+            f"{metric}_{rank}_lower",
+            f"{metric}_{rank}_point",
+            f"{metric}_{rank}_upper"
+        ]
+    ).collect().write_csv(
+        float_precision=2,
+        datetime_format="%Y-%m-%dT%H:%M"
+        )
+
+    # Write data
+    output.write(data)
+
+@app.command()
 def display(configuration: Path = Path("config.json")) -> None:
     """
     Launch graphical application in the browser. Shutdown the application using
@@ -151,71 +214,6 @@ def display(configuration: Path = Path("config.json")) -> None:
     serve_dashboards(
         configuration_file=configuration
     )
-
-# import pandas as pd
-
-# from modules.evaluate import evaluate
-# from modules.usgs import download_usgs
-# from modules.nwm import download_nwm
-# from modules.pairs import pair_nwm_usgs
-
-# def main() -> None:
-#     """Main."""
-#     serve_dashboards(
-#         configuration_file=Path("config.json")
-#     )
-#     # root = Path("/ised/nwm_explorer_data")
-
-#     # download_usgs(
-#     #     start=pd.Timestamp("2023-09-28"),
-#     #     end=pd.Timestamp("2025-10-11"),
-#     #     root=root
-#     # )
-
-#     # download_nwm(
-#     #     start=pd.Timestamp("2023-10-01"),
-#     #     end=pd.Timestamp("2025-09-30"),
-#     #     root=root,
-#     #     jobs=18,
-#     #     retries=1
-#     # )
-
-#     # pair_nwm_usgs(
-#     #     root=root,
-#     #     start_date=pd.Timestamp("2023-10-01"),
-#     #     end_date=pd.Timestamp("2025-09-30")
-#     # )
-
-#     # evaluations = {
-#     #     "FY2024Q1": (pd.Timestamp("2023-10-01"), pd.Timestamp("2023-12-31T23:59")),
-#     #     "FY2024Q2": (pd.Timestamp("2024-01-01"), pd.Timestamp("2024-03-31T23:59")),
-#     #     "FY2024Q3": (pd.Timestamp("2024-04-01"), pd.Timestamp("2024-06-30T23:59")),
-#     #     "FY2024Q4": (pd.Timestamp("2024-07-01"), pd.Timestamp("2024-09-30T23:59")),
-#     #     "FY2025Q1": (pd.Timestamp("2024-10-01"), pd.Timestamp("2024-12-31T23:59")),
-#     #     "FY2025Q2": (pd.Timestamp("2025-01-01"), pd.Timestamp("2025-03-31T23:59")),
-#     #     "FY2025Q3": (pd.Timestamp("2025-04-01"), pd.Timestamp("2025-06-30T23:59")),
-#     #     "FY2025Q4": (pd.Timestamp("2025-07-01"), pd.Timestamp("2025-09-30T23:59")),
-#     #     "FY2024": (pd.Timestamp("2023-10-01"), pd.Timestamp("2024-09-30T23:59")),
-#     #     "FY2025": (pd.Timestamp("2024-10-01"), pd.Timestamp("2025-09-30T23:59")),
-#     #     "FY2024-FY2025": (pd.Timestamp("2023-10-01"), pd.Timestamp("2025-09-30T23:59"))
-#     # }
-
-#     # for l, (s, e) in evaluations.items():
-#     #     evaluate(
-#     #         label = l,
-#     #         root = root,
-#     #         start_time = s,
-#     #         end_time = e,
-#     #         processes = 18,
-#     #         sites_per_chunk = 500
-#     #     )
-#     # evals = load_metrics(
-#     #     root=root,
-#     #     label="FY2024Q3",
-#     #     configuration=ModelConfiguration.MEDIUM_RANGE_MEM_1,
-#     #     metric=Metric.KLING_GUPTA_EFFICIENCY
-#     # )
-#     # print(evals)
 
 if __name__ == "__main__":
     # Run the CLI.
